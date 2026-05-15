8aa4f55cf27869a50fa42b660118a619efd41465
J.M. Sánchez <sanchezjm960@gmail.com>
2026-05-15 20:12:19 -0300

    Another update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         port: "",
       },
       {
-        hostname: 'energized-corgi-814.convex.site',
+        hostname: 'energized-corgi-814.convex.cloud',
         protocol: 'https',
         port: "",
       }
